fe23bd4d112654dc23a972515daf4c1c9d86c87b
Vishal Biswas <34629291+CyberKnight00@users.noreply.github.com>
2020-01-06 09:46:14 +0530

    Update wpa-psk-gen.py

--- WPA-PSK-GEN/wpa-psk-gen.py
+++ WPA-PSK-GEN/wpa-psk-gen.py
@@ -2,9 +2,9 @@
 from passlib.utils import pbkdf2
 import binascii, sys
 
-# usage wpa-psk-genpy <Passphrase> <SSID> 
+# usage wpa-psk-gen.py <Passphrase> <SSID> 
 try :
 	print("PSK = ",binascii.hexlify(pbkdf2.pbkdf2(str.encode(sys.argv[1]), str.encode(sys.argv[2]), 4096, 32)).decode("utf-8"))
 
 except:
-	print("Usage wpa-psk-genpy <Passphrase> <SSID>")
+	print("Usage wpa-psk-gen.py <Passphrase> <SSID>")
